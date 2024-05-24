4457a4b53c6695fa60aa77222e2d4b4d248c9a9f
Yashsaini27 <90746071+Yashsaini27@users.noreply.github.com>
2024-05-24 10:24:22 +0530

    Update Review.jsx

--- School-Management/src/components/Review.jsx
+++ School-Management/src/components/Review.jsx
@@ -10,7 +10,7 @@ const Review = () => {
 
     const fetchReviews = async () => {
         try {
-            const response = await axios.get('http://localhost:3000/querry/reviews');
+            const response = await axios.get('https://school-management-server-d308.onrender.com/querry/reviews');
             setReviews(response.data);
         } catch (error) {
             console.error('Error fetching reviews:', error);
